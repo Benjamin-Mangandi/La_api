99cf63009dd234b8e2ba8ccee1d5696c3000ca4c
Benjamin-Mangandi <110005569+Benjamin-Mangandi@users.noreply.github.com>
2023-03-28 11:19:56 -0600

    Actu1

--- Apis.py
+++ Apis.py
@@ -1,5 +1,4 @@
 from flask import Flask, Request, jsonify, request
-import random
 from flask_cors import CORS
 
 
@@ -82,72 +81,6 @@ def crear_usuario():
     }
     return jsonify(respuesta)
 
-#STIKERS
-
-#CARGAR STIKERS
-@app.route('/stiker', methods=["POST"])
-def cargar_stikers():
-    stikers_por_cargar = request.get_json(True)
-    if len(stikers)>0:
-        for stiker in stikers_por_cargar:
-            for stikercargados in stikers:
-                if stiker["id_stiker"] == stikercargados["id_stiker"]:
-                    stikers_por_cargar.remove(stiker)
-    for stiker in stikers_por_cargar:
-        stikers.append(stiker)
-    respuesta = {
-    "msg": "Stikers cargados con exito!",
-    "status": 200
-    }
-    return jsonify(respuesta)
-
-#OBTENER STIKER
-@app.route('/stiker', methods=["GET"])
-def buscar_stikers():
-    nombre = request.args.get("name")
-    apellido = request.args.get("lastname")
-    coincidencias_stiker =[]
-    for stiker in stikers:
-        if stiker["stiker_player_name"]==nombre and stiker["stiker_player_lastname"]==apellido:
-            coincidencias_stiker.append(stiker)
-    return jsonify(coincidencias_stiker)
-
-#ELIMINAR STIKER
-@app.route('/stiker/<int:id>', methods=['DELETE'])
-def eliminar_stiker(id):
-    for stiker in stikers:
-        if stiker["id_stiker"] == id:
-            stikers.remove(stiker)
-            respuesta = {
-                "msg": "Stiker eliminado con exito.",
-                "status": 200
-            }
-            return jsonify(respuesta)
-    respuesta_negativa ={
-        "msg": "No se encontró el Stiker, por favor verifique el ID.",
-        "status": 404
-    }
-    return jsonify(respuesta_negativa)
-
-#SOBRES
-@app.route('/box/<user>', methods=['GET'])
-def conseguir_stikers(user):
-    for usuario in usuarios:
-        if usuario["user_nickname"]==user:
-            auxiliar_stikers =[]
-            while len(auxiliar_stikers)<5:
-                numero_random = random.randint(0, 1000)
-                for stiker in stikers:
-                    if stiker["id_stiker"]==numero_random:
-                        auxiliar_stikers.append(stiker)
-                        usuario["user_stikers"].append(stiker)
-                        break
-            return jsonify(auxiliar_stikers)
-    respuesta_negativa ={
-            "msg": "No se encontró el usuario, por favor verifique el Nickname",
-            "status": 404
-        }
-    return jsonify(respuesta_negativa)
 
 #INICIAR
 if __name__ == ('__main__'):
